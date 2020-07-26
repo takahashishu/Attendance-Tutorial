13db865b07254fb13acc5c94614f02efe7d7c178
高橋　秀 <st1323.www.yoru@gmail.com>
2020-07-26 13:00:37 +0000

    7.1.4終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,13 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  # 永続的セッションを破棄します。
+  def forget(user)
+    user.forget #Userモデル参照
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   #セッションと@current_userを破棄します。
   def log_out
     session.delete(:user_id)
