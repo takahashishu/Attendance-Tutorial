8b439b8d37fff4c890421c28774dfad67ade4c45
高橋　秀 <st1323.www.yoru@gmail.com>
2020-07-26 12:45:13 +0000

    7.1.3終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  # 「remember_token」という仮想の属性を作成します。
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
@@ -9,4 +11,29 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
   
+  # 渡された文字列のハッシュ値を返します。
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返します。
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続的セッションのためハッシュ化したトークンをデータベースに記憶します。
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
