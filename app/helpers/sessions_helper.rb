0bd7f3ac2243a2b0b11b7be2f0d5fb696eeae389
高橋　秀 <st1323.www.yoru@gmail.com>
2020-07-28 21:33:22 +0000

    Finish full-fiedged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,31 +1,32 @@
 module SessionsHelper
-  
+
   # 引数に渡されたユーザーオブジェクトでログインします。
   def log_in(user)
     session[:user_id] = user.id
   end
-  
-  # 永続的セッションを記憶します（Userモデルを参照)
+
+  # 永続的セッションを記憶します（Userモデルを参照）
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-  
-  # 永続的セッションを破棄します。
+
+  # 永続的セッションを破棄します
   def forget(user)
-    user.forget #Userモデル参照
+    user.forget # Userモデル参照
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
-  
-  #セッションと@current_userを破棄します。
+
+  # セッションと@current_userを破棄します
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
-  
-  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
+
+  # 一時的セッションにいるユーザーを返します。
   # それ以外の場合はcookiesに対応するユーザーを返します。
   def current_user
     if (user_id = session[:user_id])
@@ -38,8 +39,8 @@ module SessionsHelper
       end
     end
   end
-  
-  #現在ログイン中のユーザーがいればtrue、そうでなければelseを返します。
+
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
     !current_user.nil?
   end
