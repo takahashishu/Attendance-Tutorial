47e407080533bde7ef41783152787d7b729e8ae4
高橋　秀 <st1323.www.yoru@gmail.com>
2020-07-30 13:35:04 +0000

    8.4.1終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @users = User.all
+  end
   
   def show
   end
@@ -49,6 +53,7 @@ class UsersController < ApplicationController
     # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in? # unlessは条件式がfalseの場合のみ実行される。
+        store_location
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
